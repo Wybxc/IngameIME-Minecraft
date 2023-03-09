bc6ce276ba78135e8a4563240208d9663a2347a3
TexTrue <3140846162@qq.com>
2023-03-09 15:26:33 +0800

    logger fix

--- common/src/main/kotlin/city/windmill/ingameime/IngameIME.kt
+++ common/src/main/kotlin/city/windmill/ingameime/IngameIME.kt
@@ -1,9 +1,10 @@
 package city.windmill.ingameime
 
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 object IngameIME {
     const val MODID = "ingameime"
     const val MODNAME = "IngameIME"
-    val LOGGER = LoggerFactory.getLogger(MODNAME)
+    val LOGGER: Logger = LoggerFactory.getLogger(MODNAME)
 }
